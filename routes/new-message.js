341ce39e9a891b02307876fb48e48b20733cf827
leynad <matei_daniel@outlook.com>
2024-10-26 12:46:12 +0200

    Add parsing from form

---
+++ routes/new-message.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {messages} = require('../data/messajes')
+
+router.get("/", function (req, res, next) {
+  res.render("form");
+});
+
+// Get message
+router.post("/", function (req, res, next) {
+  console.log(req.body.message);
+  console.log(req.body.user);
+
+  console.log(req.body)
+  messages.push({ text: req.body.message, user: req.body.user, added: new Date() });
+
+  res.redirect("/");
+});
+
+module.exports = router;
